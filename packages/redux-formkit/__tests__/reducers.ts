bbf187f57e73c9ca0cf51529b0d7237741b6664c
chrisfield <chris@christopherfield.co.uk>
2019-01-30 22:09:42 +0000

    Add reducer test for init-form-state

--- packages/redux-formkit/__tests__/reducers.ts
+++ packages/redux-formkit/__tests__/reducers.ts
@@ -1,5 +1,9 @@
-import { actionTypes } from '../src/actions';
 import reducer from '../src/reducers';
+import describeInitFormState from './reducers/initFormState';
+
+const tests = {
+  InitFormState: describeInitFormState
+}
 
 describe('reducers', () => {
   it('should initialize state to {}', () => {
@@ -17,28 +21,8 @@ describe('reducers', () => {
     expect(reducer(state, {type: 'SOMETHING_ELSE', form: 'foo'})).toBe(state)
   })
 
-  it('should not modify state when known action has no form', () => {
-    const state = {foo: 'bar'}
-    expect(reducer(state, {type: actionTypes.INIT_FORM_STATE})).toBe(state)
+  Object.keys(tests).forEach(key => {
+    describe(key, tests[key])
   })
-
-  it('should initialize form state when known action has form', () => {
-    const state = reducer(undefined, {
-      type: actionTypes.INIT_FORM_STATE,
-      form: 'myForm'
-    })
-    expect(state).toEqual({
-      myForm: {
-        formStatus: {
-          errorCount: 0,
-          isSubmitting: false,
-          isValid: true
-        },
-        fieldStatus: {},
-        fieldValues: {},
-        fieldErrors: {}          
-      }
-    })
-  })  
 
 })
---
+++ packages/redux-formkit/__tests__/reducers/initFormState.ts
@@ -0,0 +1,96 @@
+import { actionTypes } from '../../src/actions'
+import reducer from '../../src/reducers'
+
+const describeInitFormState = () => {
+
+  it('should not modify state when action has no form', () => {
+    const state = {foo: 'bar'}
+    expect(reducer(state, {type: actionTypes.INIT_FORM_STATE})).toBe(state)
+  })
+
+  it('should initialize form state when action has a form', () => {
+    const state = reducer(undefined, {
+      type: actionTypes.INIT_FORM_STATE,
+      form: 'myForm'
+    })
+    expect(state).toEqual({
+      myForm: {
+        formStatus: {
+          errorCount: 0,
+          isSubmitting: false,
+          isValid: true
+        },
+        fieldStatus: {},
+        fieldValues: {},
+        fieldErrors: {}          
+      }
+    })
+  })
+
+  it('should include any action formStatus, fieldStatus, fieldValues and fieldErrors in the new state', () => {
+    const state = reducer(undefined, {
+      type: actionTypes.INIT_FORM_STATE,
+      form: 'myForm',
+      formStatus: {one: '1st'},
+      fieldStatus: {two: '2nd'},
+      fieldValues: {three: '3rd'},
+      fieldErrors: {four: '4th'}
+    })
+    expect(state).toEqual({
+      myForm: {
+        formStatus: {
+          errorCount: 0,
+          isSubmitting: false,
+          isValid: true,
+          one: '1st'
+        },
+        fieldStatus: {two: '2nd'},
+        fieldValues: {three: '3rd'},
+        fieldErrors: {four: '4th'}
+      }
+    })
+  })
+
+  it('should have formStatus.isValid if errorCount is zero', () => {
+    const state = reducer(undefined, {
+      type: actionTypes.INIT_FORM_STATE,
+      form: 'myForm',
+      formStatus: {errorCount: 0}
+    })
+    expect(state).toEqual({
+      myForm: {
+        formStatus: {
+          errorCount: 0,
+          isSubmitting: false,
+          isValid: true,
+        },
+        fieldStatus: {},
+        fieldValues: {},
+        fieldErrors: {}
+      }
+    })
+  })  
+
+  it('should not have formStatus.isValid if errorCount is not zero', () => {
+    const state = reducer(undefined, {
+      type: actionTypes.INIT_FORM_STATE,
+      form: 'myForm',
+      formStatus: {errorCount: 1}
+    })
+    expect(state).toEqual({
+      myForm: {
+        formStatus: {
+          errorCount: 1,
+          isSubmitting: false,
+          isValid: false,
+        },
+        fieldStatus: {},
+        fieldValues: {},
+        fieldErrors: {}
+      }
+    })
+  })  
+
+}
+
+export default describeInitFormState
