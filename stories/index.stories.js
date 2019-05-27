15710123f7500747f4064ed533b6c668ddd6fd19
Chris Field <chris.field@thehutgroup.com>
2019-05-27 11:23:38 +0100

    Improve docs

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,16 +1,21 @@
 import { storiesOf } from '@storybook/react';
+
 import introduction from './introduction';
 import simpleForm from './introduction/simple-form';
 import arrayForm from './introduction/array-form';
+
 import field from './example-ui-components/field';
 import textInput from './example-ui-components/text-input';
 import numberInput from './example-ui-components/number-input';
 import checkbox from './example-ui-components/checkbox';
 import radioButton from './example-ui-components/radio-button';
+
 import apiForFormStateProvider from './api/form-state-provider';
 import apiForForm from './api/form';
 import apiForField from './api/field';
 import apiForHooks from './api/hooks';
+
+import nextJs from './next-js';
 
 storiesOf('Overview', module)
 .add('Getting Started', introduction)
@@ -29,3 +34,6 @@ storiesOf('Api', module)
 .add('Form', apiForForm)
 .add('Field', apiForField)
 .add('Hooks', apiForHooks);
+
+storiesOf('Next-js', module)
+.add('Server Rendering', nextJs)
---
+++ stories/next-js/index.js
@@ -0,0 +1,3 @@
+import { doc } from 'storybook-readme';
+import readme from './index.md'
+export default doc(readme);
